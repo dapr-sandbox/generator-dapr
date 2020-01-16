c1c241db22b81a974e1b59776b75941b04b67b51
unknown <ryanvolum@gmail.com>
2020-01-16 12:52:10 -0800

    added filepaths for bindings

--- src/files.ts
+++ src/files.ts
@@ -2,12 +2,16 @@ import { LanguageTemplates, ComponentTemplates } from './types';
 
 export const componentTemplates: ComponentTemplates = {
     "Redis": {
-        componentName: "redis",
-        manifestPath: "state/redis.yaml"
+        componentName: "redis-state",
+        manifestPath: "state/redis-state.yaml"
+    },
+    "Redis Binding": {
+        componentName: "redis-binding",
+        manifestPath: "bindings/redis-binding.yaml"
     },
     "Azure CosmosDB": {
-        componentName: "cosmosdb",
-        manifestPath: "state/cosmosdb.yaml"
+        componentName: "cosmosdb-state",
+        manifestPath: "state/cosmosdb-state.yaml"
     },
     "NATS": {
         componentName: "nats-pubsub",
@@ -17,6 +21,10 @@ export const componentTemplates: ComponentTemplates = {
         componentName: "rabbitmq-pubsub",
         manifestPath: "pubsub/rabbitmq-pubsub.yaml"
     },
+    "RabbitMQ Binding": {
+        componentName: "rabbitmq-binding",
+        manifestPath: "bindings/rabbitmq-binding.yaml"
+    },
     "Redis Streams": {
         componentName: "redis-pubsub",
         manifestPath: "pubsub/redis-pubsub.yaml"
@@ -24,6 +32,66 @@ export const componentTemplates: ComponentTemplates = {
     "Azure Service Bus": {
         componentName: "servicebus-pubsub",
         manifestPath: "pubsub/servicebus-pubsub.yaml"
+    }, 
+    "AWS DynamoDB Binding": {
+        componentName: "aws-dynamodb-binding",
+        manifestPath: "bindings/aws-dynamodb-binding.yaml"
+    },
+    "AWS S3 Binding": {
+        componentName: "aws-s3-binding",
+        manifestPath: "bindings/aws-s3-binding.yaml"
+    },
+    "AWS SNS Binding": {
+        componentName: "aws-sns-binding",
+        manifestPath: "bindings/aws-sns-binding.yaml"
+    },
+    "AWS SQS Binding": {
+        componentName: "aws-sqs-binding",
+        manifestPath: "bindings/aws-sqs-binding.yaml"
+    },
+    "Azure Blob Storage Binding": {
+        componentName: "azure-blob-storage-binding",
+        manifestPath: "bindings/azure-blob-storage-binding.yaml"
+    },
+    "Azure CosmosDB Binding": {
+        componentName: "azure-cosmosdb-binding",
+        manifestPath: "bindings/azure-cosmosdb-binding.yaml"
+    },
+    "Azure EventHubs Binding": {
+        componentName: "azure-eventhubs-binding",
+        manifestPath: "bindings/azure-eventhubs-binding.yaml"
+    },
+    "Azure Service Bus Queues Binding": {
+        componentName: "azure-service-bus-queues-binding",
+        manifestPath: "bindings/azure-service-bus-queues-binding.yaml"
+    },
+    "Azure SignalR Binding": {
+        componentName: "azure-signalr-binding",
+        manifestPath: "bindings/azure-signalr-binding.yaml"
+    },
+    "GCP Cloud Pub/Sub Binding": {
+        componentName: "gcp-cloud-pubsub-binding",
+        manifestPath: "bindings/gcp-cloud-pubsub-binding.yaml"
+    },
+    "GCP Storage Bucket Binding": {
+        componentName: "gcp-storage-bucket-binding",
+        manifestPath: "bindings/gcp-storage-bucket-binding.yaml"
+    }, 
+    "HTTP Binding": {
+        componentName: "http-binding",
+        manifestPath: "bindings/http-binding.yaml"
+    },
+    "Kafka Binding": {
+        componentName: "kafka-binding",
+        manifestPath: "bindings/kafka-binding.yaml"
+    },
+    "Kubernetes Events Binding": {
+        componentName: "kubernetes-events-binding",
+        manifestPath: "bindings/kubernetes-events-binding.yaml"
+    },
+    "MQTT Binding": {
+        componentName: "mqtt-binding",
+        manifestPath: "bindings/mqtt-binding.yaml"
     }
 }
 
